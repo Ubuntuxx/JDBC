d2738da1f79e571008f939dccde7003855d936fa
JLAGUbuntu <unixluisx@protonmail.ch>
2023-05-22 22:32:33 -0600

    1st Test database!

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/springboot", "pepe", "luis");
+            Statement instruction = conn.createStatement();
+            String sql = "Select * from nombres";
+            ResultSet resuktado = instruction.executeQuery(sql);
+            while (resuktado.next()){
+                System.out.println("Id: " + resuktado.getInt("id") + "\t\tnombre " + resuktado.getString("nombre") + "\t\tapellido " + resuktado.getString("apellido"));
+            }
+            resuktado.close();
+            instruction.close();
+            conn.close();
+        } catch (SQLException e){
+            e.printStackTrace(System.out);
+        }
     }
 }
